5cd7dc120ac9458fd74d41bebf630d17a05439eb
Huub de Beer <Huub@heerdebeer.org>
2018-05-10 16:13:50 +0200

    Started working on the ScoreCard component

---
+++ src/score_card/ScoreCard.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2018 Huub de Beer
+ *
+ * This file is part of twenty-one-pips.
+ *
+ * Twenty-one-pips is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Twenty-one-pips is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with twenty-one-pips.  If not, see <http://www.gnu.org/licenses/>.
+ * @ignore
+ */
+import {ConfigurationError} from "../error/ConfigurationError.js";
+import {ViewController} from "../ViewController.js";
+import {DEFAULT_SYSTEM_PLAYER} from "../Player.js";
+import {Die} from "../Die.js";
+
+/**
+ * @module
+ */
+
+
+/**
+ * ScoreCard is a component to render the scores per player in a game.
+ *
+ * @extends module:ViewController~ViewController
+ */
+const ScoreCard = class extends ViewController {
+
+    /**
+     * Create a new PlayingTable component.
+     *
+     * @param {Object} config - The initial configuration of the new ScoreCard
+     */
+    constructor({
+        parent = null,
+    } = {}) {
+        super({parent});
+        this.element.classList.add("score-card");
+    }
+
+};
+
+export {
+    ScoreCard
+};
