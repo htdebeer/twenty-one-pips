3cf43fed87969f7abbfc91a075ae3096790355b6
Huub de Beer <Huub@heerdebeer.org>
2019-01-05 12:26:03 +0100

    Updated build process with rollup+gulp+terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,32 @@
 const gulp = require("gulp");
-const uglifyEs = require("gulp-uglify-es").default;
+const terser = require("gulp-terser");
 const rename = require("gulp-rename");
-const rollup = require("gulp-rollup");
+const rollup = require("rollup-stream");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
 const sourcemaps = require("gulp-sourcemaps");
 
 const BASE_NAME = "twenty-one-pips";
 const SOURCE = "./src";
 const DESTINATION = "./lib";
+const inputFile = `${BASE_NAME}.js`;
 
 const defaultTask = function () {
-    return gulp.src(`${SOURCE}/**/*.js`)
-        .pipe(sourcemaps.init())
-            .pipe(rollup({
-                input: `${SOURCE}/${BASE_NAME}.js`,
-                output: {
-                    format: "es",
-                }
-            }))
-            .pipe(rename(`${BASE_NAME}.js`))
-            .pipe(sourcemaps.write())
-        .pipe(gulp.dest(DESTINATION))
-            .pipe(uglifyEs())
-            .pipe(rename(`${BASE_NAME}.min.js`))
-            .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest(DESTINATION));
+    return rollup({
+        input: `${SOURCE}/${inputFile}`,
+        format: "es",
+        sourcemap: true
+    })
+    .pipe(source(inputFile, SOURCE))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(rename(`${BASE_NAME}.js`))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DESTINATION))
+    .pipe(terser())
+    .pipe(rename(`${BASE_NAME}.min.js`))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest(DESTINATION));
 };
 
 exports.default = defaultTask;
